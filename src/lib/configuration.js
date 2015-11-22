a4248bb5ff2c8d71be097adfa3ac9f3c67facc2c
Tom Valk <tomvalk@gmail.com>
2015-11-22 21:55:30 +0100

    Configuration Lib. Helper for loading config and holding it.

---
+++ src/lib/configuration.js
@@ -0,0 +1,42 @@
+/**
+ * Configuration
+ */
+
+'use strict';
+
+// Imports
+var fs = require('fs');
+var yaml = require('js-yaml');
+var Joi = require('joi');
+var _ = require('underscore');
+
+var directory = require('./directory');
+
+
+var configSchema = {
+    config: {
+        debug: Joi.boolean(),
+        server: {
+            address: Joi.string().required(),
+            port: Joi.number().required(),
+            authentication: {
+                username: Joi.string().required(),
+                password: Joi.string().required()
+            }
+        },
+        db: Joi.any()
+    },
+    plugins: Joi.array()
+};
+
+var config = yaml.safeLoad(fs.readFileSync(directory.rootPath() + 'config.yaml', 'utf8'));
+
+module.exports = {};
+_.extend(module.exports, config);
+module.exports.validate = function() {
+    var res = Joi.validate(this._config, configSchema);
+    if(res.error) {
+        throw res.error;
+    }
+    return true;
+};
