0328a7a7418dee7605d9fd836065e1955dbd35b7
Tom Valk <tomvalk@gmail.com>
2015-11-22 21:55:06 +0100

    Directory Lib. Helper for absolute paths.

---
+++ src/lib/directory.js
@@ -0,0 +1,16 @@
+
+var fs = require('fs');
+var path = require('path');
+
+module.exports = {
+    rootPath: function() {
+        return path.normalize(__dirname + "/../../");
+    },
+    srcPath: function() {
+        return path.normalize(__dirname + "/../");
+    },
+    pluginsPath: function() {
+        return path.normalize(__dirname + "/../../plugins/");
+    },
+    sep: path.sep
+};
