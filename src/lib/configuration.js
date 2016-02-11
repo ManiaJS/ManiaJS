222ad753f83c8ca42d7be8af2f8acd8424284789
Tom Valk <tomvalk@gmail.com>
2016-02-11 11:09:50 +0100

    Reformat code, use tab size 2 spaces.

--- src/lib/configuration.js
+++ src/lib/configuration.js
@@ -14,52 +14,52 @@ var directory = require('./directory');
 
 
 var configSchema = {
-    config: {
-        debug: Joi.boolean(),
-        server: {
-            address: Joi.string().required(),
-            port: Joi.number().required(),
-            authentication: {
-                username: Joi.string().required(),
-                password: Joi.string().required()
-            }
-        },
-        db: {
-            dialect: Joi.array().items(Joi.string().valid('mysql', 'mariadb', 'sqlite')),
-            database: Joi.string(),
-            authentication: {
-                username: Joi.string(),
-                password: Joi.string()
-            },
-            pool: {
-                max: Joi.number().required(),
-                min: Joi.number().required(),
-                idle: Joi.number().required()
-            },
-            mysql: {
-                host: Joi.string().required(),
-                port: Joi.number().required()
-            },
-            mariadb: {
-                host: Joi.string().required(),
-                port: Joi.number().required()
-            },
-            sqlite: {
-                storage: Joi.string().required()
-            }
-        }
+  config: {
+    debug: Joi.boolean(),
+    server: {
+      address: Joi.string().required(),
+      port: Joi.number().required(),
+      authentication: {
+        username: Joi.string().required(),
+        password: Joi.string().required()
+      }
     },
-    plugins: Joi.array()
+    db: {
+      dialect: Joi.array().items(Joi.string().valid('mysql', 'mariadb', 'sqlite')),
+      database: Joi.string(),
+      authentication: {
+        username: Joi.string(),
+        password: Joi.string()
+      },
+      pool: {
+        max: Joi.number().required(),
+        min: Joi.number().required(),
+        idle: Joi.number().required()
+      },
+      mysql: {
+        host: Joi.string().required(),
+        port: Joi.number().required()
+      },
+      mariadb: {
+        host: Joi.string().required(),
+        port: Joi.number().required()
+      },
+      sqlite: {
+        storage: Joi.string().required()
+      }
+    }
+  },
+  plugins: Joi.array()
 };
 
 var config = yaml.safeLoad(fs.readFileSync(directory.rootPath() + 'config.yaml', 'utf8'));
 
 module.exports = {};
 _.extend(module.exports, config);
-module.exports.validate = function() {
-    var res = Joi.validate(this._config, configSchema);
-    if(res.error) {
-        throw res.error;
-    }
-    return true;
+module.exports.validate = function () {
+  var res = Joi.validate(this._config, configSchema);
+  if (res.error) {
+    throw res.error;
+  }
+  return true;
 };
