748df10937224a4c2fe8077cbc9667e500605a40
Tom Valk <tomvalk@gmail.com>
2016-02-12 19:41:56 +0100

    Discussion, own plugin manager? (prepare for it with this commit).

--- maniajs.js
+++ maniajs.js
@@ -1,5 +1,9 @@
 /**
  * ManiaJS Startup Script
+ *
+ * @license ICS {LICENSE file}
+ * @author ManiaJS
+ * @version 0.0.0
  */
-require('babel-register');
-require('./src/maniajs');
+require('babel-register'); // Require hook, babel
+require('./src/maniajs');  // Start ManiaJS
---
+++ src/bin/mpm.js
@@ -0,0 +1,3 @@
+/**
+ * ManiaJS Package Manager
+ */
