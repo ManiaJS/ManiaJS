3c90d6ee27014f36995e84eba3a0a208683df6e3
Tom Valk <tomvalk@gmail.com>
2016-02-13 20:03:45 +0100

    Prepare database client and update config.

--- src/database/facade.js
+++ src/database/facade.js
@@ -14,7 +14,7 @@ export default class extends Facade {
   constructor(app) {
     super(app);
 
-    this.client = new Client();
+    this.client = new Client(app, app.config.db);
   }
 
   /**
@@ -22,6 +22,29 @@ export default class extends Facade {
    * @returns {Promise}
    */
   init() {
-    return Promise.resolve();
+    this.app.log.debug("Connecting to database...");
+
+    return this.client.connect();
+  }
+
+
+  run() {
+    this.app.log.debug("Registering models...");
+    this.defineModels();
+
+    this.app.log.debug("Syncing models with database...");
+
+  }
+
+
+  /**
+   * Define Models of ManiaJS and Plugins!
+   */
+  defineModels() {
+    // First the core models.
+
+
+    // Plugin models.
+    this.app.pluginFacade.manager.loadModels(this.client.sequelize);
   }
 }
