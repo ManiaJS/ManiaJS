ce6902e23a6b217ff6131ce1cae495b02e754fcd
Tom Valk <tomvalk@gmail.com>
2016-02-14 11:59:03 +0100

    Adding ability to have custom parse function in register of gbx to events.

--- src/server/callback-manager.js
+++ src/server/callback-manager.js
@@ -29,6 +29,7 @@ export default class {
    * @param {string} options.callback Callback name that will be fired by the GBX Client.
    * @param {string} options.event Converting into event name.
    * @param {object} options.parameters Parameters mapping.
+   * @param {function} options.parse Custom parse mapping function.
    * @param {object} options.game Optional game strings. Provide the titleid of the parent title (the game title).
    */
   register(options) {
@@ -37,27 +38,35 @@ export default class {
     let callbackName = options.callback;
     let eventName = options.event;
     let parameters = options.parameters;
+    let parse = options.parse;
     let game = options.game || []; // Default all games
 
     // Register callback, make it an event.
     console.log(callbackName);
 
     this.client.gbx.on(callbackName, function(rawParams) {
-      var params = util._extend({}, parameters); // (clone it).
-      let paramKeys = Object.keys(parameters);
+      // Output var
+      var params = {};
 
-      for (var i = 0; i < paramKeys.length; i++) {
-        let key = paramKeys[i];
-        let num = parameters[paramKeys[i]];
+      // If we have custom parser, then call it, if not we go with the mapping.
+      if (typeof parse === 'function') {
+        params = parse(rawParams);
+      } else {
+        params = util._extend({}, parameters); // (clone it).
+        let paramKeys = Object.keys(parameters);
 
-        // Do we have that num in our rawParams?
-        if (num < rawParams.length) {
-          // Allrighty!
-          // Map it.
-          params[key] = rawParams[num];
+        for (var i = 0; i < paramKeys.length; i++) {
+          let key = paramKeys[i];
+          let num = parameters[paramKeys[i]];
+
+          // Do we have that num in our rawParams?
+          if (num < rawParams.length) {
+            // Allrighty!
+            // Map it.
+            params[key] = rawParams[num];
+          }
         }
       }
-
 
       // Trigger the event on our client
       self.client.emit(eventName, params);
