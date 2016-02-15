87f88997730d20d0e73620cd3837ef521927b7f3
Tom Valk <tomvalk@gmail.com>
2016-02-15 12:16:05 +0100

    Precompile before running, requires less memory.

--- maniajs.js
+++ maniajs.js
@@ -3,7 +3,5 @@
  *
  * @license ICS {LICENSE file}
  * @author ManiaJS
- * @version 0.0.0
  */
-require('babel-register')({ignore:false}); // Require hook, babel
-require('./src/maniajs');                  // Start ManiaJS
+require('./lib/maniajs');
