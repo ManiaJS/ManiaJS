65fe3388b159e2fbdd6636987637c84fb04dd5db
Kristjan Broder Lund <kristjan.1234@gmail.com>
2016-07-05 20:12:56 +0000

    Always show ms in time to string parse

--- src/Util/Times.ts
+++ src/Util/Times.ts
@@ -13,12 +13,5 @@ export function stringTime (input) {
   let sec = Math.floor((input - min * 60 * 1000) / 1000);
   let msec = input % 1000;
 
-  if(msec) {
-    if(min > 9) {
-      return sprintf('%02d:%02d.%03d', min, sec, msec);
-    } else {
-      return sprintf('%01d:%02d.%03d', min, sec, msec);
-    }
-  }
-  return sprintf('%02d:%02d', min, sec);
+  return sprintf('%01d:%02d.%03d', min, sec, msec);
 }
