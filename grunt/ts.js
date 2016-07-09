398f7804f2e49610c2d1a3fa0b59674916ab7fce
Tom Valk <tomvalk@gmail.com>
2016-07-09 23:08:54 +0200

    Build fixes.

--- grunt/ts.js
+++ grunt/ts.js
@@ -6,26 +6,9 @@ module.exports = {
 
     files: [
       {
-        src: ['src/**.ts'],
+        src: ['src/**.ts', 'src/*/**.ts'],
         dest: 'lib'
       }
     ]
-  }
-};
-
-let t = {
-  files: [
-    {
-      src: ['src/**.ts'],
-      dest: 'lib'
-    },
-    // { src: 'test/**.ts' , dest: 'lib/test' }
-  ],
-
-  options: {
-    // tsconfig: __dirname + '/../../tsconfig.json',
-    // fast: 'never',
-    // module: 'es6',
-    // target: 'es6'
   }
 };
