d183a344a87b5b2f996f395f259c9866f0c66aa4
Tom Valk <tomvalk@gmail.com>
2016-07-28 14:39:00 +0200

    Expose the export js to dependencies.

---
+++ src/Export.ts
@@ -0,0 +1,6 @@
+/**
+ * Export Interface.
+ * This will be exposed to plugins using the core sandboxes.
+ * The export will expose a base class for the plugin itself and some useful utils.
+ */
+export {Sandbox} from './Plugin/Sandbox';
--- src/Plugin/Mediator.ts
+++
@@ -1,3 +0,0 @@
-/**
- * Created by tom on 28-7-2016.
- */
